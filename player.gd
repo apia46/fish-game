9c0210392f13c738a8fa7bf85e3a9c57ce28cea5
Apia <beekayt274@gmail.com>
2026-02-10 14:41:58 +1300

    basic mechanics

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+@onready var bar:Panel = get_parent()
+
+const HALF_BAR_HEIGHT:float = 25
+
+var velocity:Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	velocity.y += delta * 100 # gravity
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT): velocity.y += delta * -300
+		
+	position += velocity * delta
+
+	if position.y < HALF_BAR_HEIGHT: # bounce off top
+		position.y = HALF_BAR_HEIGHT
+		if velocity.y < 0: velocity.y *= -0.8
+	if position.y > bar.size.y - HALF_BAR_HEIGHT: # bounce off bottom
+		position.y = bar.size.y - HALF_BAR_HEIGHT
+		if velocity.y > 0: velocity.y *= -0.8
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and !event.echo:
+		match event.keycode:
+			KEY_SPACE: velocity.y *= -0.8 # dash attack
